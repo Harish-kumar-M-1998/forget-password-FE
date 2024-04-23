b332d5a961ca105d29c31d1de8a91490921ff5cc
Harish-kumar-M-1998 <harishmano98@gmail.com>
2024-04-23 19:06:13 +0530

    home login signup added

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home"
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+
+
+function App() {
+  return (
+    
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
